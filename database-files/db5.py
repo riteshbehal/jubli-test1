86bf689e95df7ae8e2a53e5b24a797606bb14619
Ritesh Behal <49110318+riteshbehal@users.noreply.github.com>
2026-06-13 20:43:32 +0530

    Add db5.py for database operations

---
+++ database-files/db5.py
@@ -0,0 +1,10 @@
+se complete authentication in your browser...
+Enumerating objects: 5, done.
+Counting objects: 100% (5/5), done.
+Delta compression using up to 20 threads
+Compressing objects: 100% (3/3), done.
+Writing objects: 100% (3/3), 295 bytes | 295.00 KiB/s, done.
+Total 3 (delta 2), reused 0 (delta 0), pack-reused 0 (from 0)
+remote: Resolving deltas: 100% (2/2), completed with 2 local objects.
+To https://github.com/riteshbehal/jubli-test1.git
+   0b57f93..e5cec46  main -> main
